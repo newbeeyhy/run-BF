73e0e6725ad96588eeb677ffda9312d2913a913d
YangHaoyu <104432982+newbeeyhy@users.noreply.github.com>
2024-07-14 23:22:06 +0800

    Add files via upload

---
+++ run-BF.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+char arr[1000];
+char *p = arr + 500;
+
+void run(char c) {
+    switch (c) {
+        case '>': p++; break;
+        case '<': p--; break;
+        case '+': *p = *p + 1; break;
+        case '-': *p = *p - 1; break;
+        case '.': cout << char(*p); break;
+        case ',': *p = getchar(); break;
+        default: break;
+    }
+}
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '[') {
+            if (*p == 0) {
+                int cnt = 1;
+                while (cnt != 0) {
+                    i++;
+                    if (s[i] == '[') cnt++;
+                    if (s[i] == ']') cnt--;
+                }
+                i--;
+            }
+        }
+        else if (s[i] == ']') {
+            if (*p != 0) {
+                int cnt = 1;
+                while (cnt != 0) {
+                    i--;
+                    if (s[i] == ']') cnt++;
+                    if (s[i] == '[') cnt--;
+                }
+                i--;
+            }
+        }
+        else run(s[i]);
+    }
+    return 0;
+}
