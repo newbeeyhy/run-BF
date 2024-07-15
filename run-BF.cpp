0030589846e12bd08601c62078053990b1b15b9c
YangHaoyu <104432982+newbeeyhy@users.noreply.github.com>
2024-07-15 11:38:53 +0800

    Add files via upload

--- run-BF.cpp
+++ run-BF.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 char arr[1000];
-char *p = arr + 500;
+char *p;
 
 void run(char c) {
     switch (c) {
@@ -18,33 +18,38 @@ void run(char c) {
 }
 
 int main() {
-    string s;
-    cin >> s;
-    int n = s.length();
-    for (int i = 0; i < n; i++) {
-        if (s[i] == '[') {
-            if (*p == 0) {
-                int cnt = 1;
-                while (cnt != 0) {
-                    i++;
-                    if (s[i] == '[') cnt++;
-                    if (s[i] == ']') cnt--;
-                }
-                i--;
-            }
-        }
-        else if (s[i] == ']') {
-            if (*p != 0) {
-                int cnt = 1;
-                while (cnt != 0) {
+    while (1) {
+        memset (arr, 0, sizeof(arr));
+        p = arr + 500;
+        cout << "\n> ";
+        string s;
+        cin >> s;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '[') {
+                if (*p == 0) {
+                    int cnt = 1;
+                    while (cnt != 0) {
+                        i++;
+                        if (s[i] == '[') cnt++;
+                        if (s[i] == ']') cnt--;
+                    }
                     i--;
-                    if (s[i] == ']') cnt++;
-                    if (s[i] == '[') cnt--;
                 }
-                i--;
             }
+            else if (s[i] == ']') {
+                if (*p != 0) {
+                    int cnt = 1;
+                    while (cnt != 0) {
+                        i--;
+                        if (s[i] == ']') cnt++;
+                        if (s[i] == '[') cnt--;
+                    }
+                    i--;
+                }
+            }
+            else run(s[i]);
         }
-        else run(s[i]);
     }
     return 0;
 }
